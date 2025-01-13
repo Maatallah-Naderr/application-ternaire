67734090b9cbb661882363c0815e3e6d6abaacd9
Maatallah-Naderr <nadermaatallah8@gmail.com>
2025-01-13 19:45:54 +0100

    ternal application

---
+++ app.js
@@ -0,0 +1,7 @@
+const bill = 270 ;
+const tip = bill >=50 && bill<= 300 ? (bill/100)*15 : (bill/100)*20 ;
+console.log(`the tip is : ${tip}`);
+
+const total = console.log(`le bill est ${bill} le tip est ${tip} le total ${bill+ tip}`);
+
+
